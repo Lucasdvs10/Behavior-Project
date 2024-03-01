7acad147129403015e3636e369714e1002edab9c
Lucas Duez <lucassantos@cremesp.org.br>
2024-03-01 19:27:14 -0300

    So pode pular se estiver no chao

--- Assets/Scripts/Movements/Jump.cs
+++ Assets/Scripts/Movements/Jump.cs
@@ -5,14 +5,23 @@ namespace DefaultNamespace {
     public class Jump : MonoBehaviour {
         [SerializeField] private Vector2 _jumpForce;
         private Rigidbody2D _rgb;
+        private bool _isJumping;
 
         private void Awake() {
             _rgb = GetComponent<Rigidbody2D>();
         }
 
+        private void OnCollisionEnter2D(Collision2D other) {
+            if (other.gameObject.CompareTag("Ground")) {
+                _isJumping = false;
+            }
+        }
 
         public void JumpAction() {
+            if (_isJumping) return;
+            
             _rgb.AddForce(_jumpForce, ForceMode2D.Impulse);
+            _isJumping = true;
         }
         
     }

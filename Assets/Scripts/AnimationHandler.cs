1b912083f6438ca3cd365efaa76e88b38bef0639
Lucas Duez <lucassantos@cremesp.org.br>
2024-03-04 18:09:07 -0300

    animacoes de andar e pular implementadas

---
+++ Assets/Scripts/AnimationHandler.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+
+namespace DefaultNamespace {
+    public class AnimationHandler : MonoBehaviour {
+        private Animator _animator;
+
+        private void Awake() {
+            _animator = GetComponent<Animator>();
+        }
+        
+        public void SetGroundedFlag(bool isGrounded) {
+            _animator.SetBool("Grounded", isGrounded);
+        }
+
+        public void SetAnimationState(int animationState) {
+            _animator.SetInteger("AnimationState", animationState);
+        }
+        
+        public void SetAnimation(string animationName) {
+            _animator.Play(animationName);
+        }
+
+        public void FlipSprite(int direction) {
+            transform.rotation = Quaternion.Euler(1,direction,1);
+        }
+        
+        private void OnCollisionEnter2D(Collision2D other) {
+            if (other.gameObject.CompareTag("Ground")) {
+                SetGroundedFlag(true);
+                SetAnimation("Idle");
+            }
+        }
+    }
+}

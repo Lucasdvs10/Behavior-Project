89022228af163615a1f6e1db694f6c1d43ea8cba
Lucas Duez <lucassantos@cremesp.org.br>
2024-03-04 18:31:40 -0300

    animacao de tomar dano implementada

--- Assets/Scripts/LifeSystem.cs
+++ Assets/Scripts/LifeSystem.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 namespace DefaultNamespace {
     public class LifeSystem : MonoBehaviour {
         [SerializeField] private int _maxLife;
-        SOBaseGlobalEvent _onDeathEvent;
-        SOBaseGlobalEvent _onDamageEvent;
+        [SerializeField] SOBaseGlobalEvent _onDeathEvent;
+        [SerializeField] SOBaseGlobalEvent _onDamageEvent;
         int _currentLife;
         
         private void Awake() {
             _currentLife = _maxLife;
+        }
+
+        [ContextMenu("TestDamage")]
+        public void TestDamage() {
+            TakeDamage(1);
         }
         
         public void TakeDamage(int damage) {
